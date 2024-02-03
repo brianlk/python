39f1eb50c9d8a2335c04114fb6e00dcc6f4a7713
Brian <brian.leungkp@gmail.com>
2024-02-03 21:57:55 +0800

    add logo

--- pygame_gui/main.py
+++ pygame_gui/main.py
@@ -1,15 +1,24 @@
 import pygame
 from pygame.locals import *
 import sys
+import random
 
 BLACK = (100, 100, 100)
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 FRAMES_PER_SEC = 30
+BALL_WIDTH_HEIGHT = 100
+MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
+MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
 
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
+ballImage = pygame.image.load('images/ithome_logo_0.png')
+
+ballX = random.randrange(MAX_WIDTH)
+ballY = random.randrange(MAX_HEIGHT)
+ballRect = pygame.Rect(ballX, ballY, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
 
 while True:
     for e in pygame.event.get():
@@ -18,5 +27,6 @@ while True:
             sys.exit()
 
     window.fill(BLACK)
+    window.blit(ballImage, (ballX, ballY))
     pygame.display.update()
     clock.tick(FRAMES_PER_SEC)
