4b1fa9e359de77d2736659221f4b845b51182bd8
brian <brian.leung@techdata.com>
2024-03-21 16:30:11 +0800

    updates

--- ovirt-api_py/operate_ovirt.py
+++ ovirt-api_py/operate_ovirt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import requests
 import xml.etree.ElementTree as ET
 
@@ -42,6 +43,25 @@ def post_api(url: str):
     return private_url_response_xml.status_code
 
 
+
+def snapshot_api(url: str):
+    ovirt_url = url
+    username = "admin@internal"
+    password = "password"
+    headers = {'Content-Type': 'application/xml'}
+    xml = """<?xml version='1.0' encoding='utf-8'?>
+    <snapshot><description>vm snapshot</description></snapshot>"""
+    private_url_response_xml = requests.post(
+        url=ovirt_url,
+        auth=HTTPBasicAuth(username, password),
+        verify="ca.pem",
+        headers=headers,
+        data=xml
+    )
+
+    # Print xml
+    return private_url_response_xml
+
 xml_string = get_api("https://manager2.oc.example/ovirt-engine/api/vms")
 
 root = ET.ElementTree(ET.fromstring(xml_string))
@@ -54,10 +74,14 @@ for v in all_vm:
     if v.find('name').text == vm_name:
         vid = v.get('id')
         
-shutdown_action = post_api(f"https://manager2.oc.example/ovirt-engine/api/vms/{vid}/shutdown")
+# shutdown_action = post_api(f"https://manager2.oc.example/ovirt-engine/api/vms/{vid}/shutdown")
 
-print(shutdown_action)
+# print(shutdown_action)
 
-shutdown_action = post_api(f"https://manager2.oc.example/ovirt-engine/api/vms/{vid}/start")
+# shutdown_action = post_api(f"https://manager2.oc.example/ovirt-engine/api/vms/{vid}/start")
 
-print(shutdown_action)
+# print(shutdown_action)
+
+take_snapshot_action = snapshot_api(f"https://manager2.oc.example/ovirt-engine/api/vms/{vid}/snapshots")
+print(take_snapshot_action.status_code)
+print(take_snapshot_action.text)
