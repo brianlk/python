5f60fa29766971e91a3c86dc2f93e0e36abcf21e
brian <brian.leung@techdata.com>
2024-03-21 17:40:40 +0800

    updates

--- ovirt-api_py/operate_ovirt.py
+++ ovirt-api_py/operate_ovirt.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
+# Script to operte VM on oVirt thru API
+
+
 import argparse
 import requests
 import xml.etree.ElementTree as ET
 
 from requests.auth import HTTPBasicAuth
 
+USERNAME = "admin@internal"
+PASSWORD = "password"
+API_URL = "https://manager2.oc.example/ovirt-engine/api"
+
 
 def get_api(url: str):
     ovirt_url = url
-    username = "admin@internal"
-    password = "password"
+    username = USERNAME
+    password = PASSWORD
     headers = {'Content-Type': 'application/xml'} 
     private_url_response_xml = requests.get(
         url=ovirt_url,
@@ -20,13 +27,13 @@ def get_api(url: str):
     )
 
     # Print xml
-    return private_url_response_xml
+    return private_url_response_xml.text
 
 
 def post_api(url: str):
     ovirt_url = url
-    username = "admin@internal"
-    password = "password"
+    username = USERNAME
+    password = PASSWORD
     headers = {'Content-Type': 'application/xml'}
     xml = """<?xml version='1.0' encoding='utf-8'?>
     <action/>"""
@@ -45,8 +52,8 @@ def post_api(url: str):
 
 def snapshot_api(url: str):
     ovirt_url = url
-    username = "admin@internal"
-    password = "password"
+    username = USERNAME
+    password = PASSWORD
     headers = {'Content-Type': 'application/xml'}
     xml = """<?xml version='1.0' encoding='utf-8'?>
     <snapshot><description>vm snapshot</description></snapshot>"""
@@ -69,19 +76,23 @@ def main():
     args = parser.parse_args()
     vm_name = args.vm_name
     action = args.action
-    xml_string = get_api("https://manager2.oc.example/ovirt-engine/api/storagedomains/ac7623af-0409-473d-a2aa-486f43bf26d0/disksnapshots")
-    print(xml_string.text)
-
-    # root = ET.ElementTree(ET.fromstring(xml_string))
-    # all_vm = root.findall('vm')
-    # vid = None
-    # for v in all_vm:
-    #     if v.find('name').text == vm_name:
-    #         vid = v.get('id')
+    xml_string = get_api(f"{API_URL}/vms")
+    root = ET.ElementTree(ET.fromstring(xml_string))
+    all_vm = root.findall('vm')
+    vid = None
+    for v in all_vm:
+        if v.find('name').text == vm_name:
+            vid = v.get('id')
             
-    # shutdown_action = post_api(f"https://manager2.oc.example/ovirt-engine/api/vms/{vid}/shutdown")
+    URL_MAP = {
+        "shutdown": f"{API_URL}/vms/{vid}/shutdown",
+        "start": f"{API_URL}/vms/{vid}/start",
+        "snapshot": f"{API_URL}/vms/{vid}/snapshots"
+    }
+            
+    shutdown_action = post_api(URL_MAP[action])
 
-    # print(shutdown_action)
+    print(shutdown_action)
 
     # shutdown_action = post_api(f"https://manager2.oc.example/ovirt-engine/api/vms/{vid}/start")
 
